02435a22e7b8495acd6ba4eb6f98de9ca82a79c7
AFuselli <91535784+AFuselli@users.noreply.github.com>
2021-12-01 16:44:30 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,18 @@
+//
+// Created by Andrea Fuselli on 01/12/21.
+//
+
+#include "LibreriaLettere.h"
+char funzione(char a){
+    if((65<=a and a<=90)){
+        a=a+32;
+        return a;
+    }else if(97<=a and a<=122){
+        a=a-32;
+        return a;
+    }else{
+        return 0;
+    }
+
+}
 
